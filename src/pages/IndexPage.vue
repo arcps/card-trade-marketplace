41200c0dd54c8905d5f38bcba4ce8b4f80cd5598
Alexandre <alexandrerytchko@hotmail.com>
2026-03-05 18:37:56 -0300

    Fix home trades fetch on page change

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -15,7 +15,7 @@
     <div v-else class="q-gutter-md">
       <TradeCard v-for="trade in trades" :key="trade.id" :trade="trade" />
 
-      <div v-if="hasMore" class="flex justify-center q-mt-lg">
+      <div v-if="hasNextPage" class="flex justify-center q-mt-lg">
         <q-btn flat color="primary" label="Carregar mais" @click="loadMoreTrades" />
       </div>
     </div>
@@ -23,41 +23,32 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import { useQuery } from '@tanstack/vue-query';
+import { computed } from 'vue';
+import { useInfiniteQuery } from '@tanstack/vue-query';
 import { getTrades } from 'src/services/api/trades';
-import type { Trade } from 'src/services/api/trades';
 import { useI18n } from 'vue-i18n';
 import TradeCard from 'components/tradeCard.vue';
 
 const { t } = useI18n();
 
-const currentPage = ref(1);
 const rpp = 10;
-const allTrades = ref<Trade[]>([]);
 
-const queryKey = computed(() => ['trades', currentPage.value, rpp]);
-
-const { isLoading } = useQuery({
-  queryKey,
-  queryFn: async () => {
-    const response = await getTrades(currentPage.value, rpp);
-    if (currentPage.value === 1) {
-      allTrades.value = response.data.list;
-    } else {
-      allTrades.value.push(...response.data.list);
-    }
-    hasMore.value = response.data.more;
+const { data, isLoading, hasNextPage, fetchNextPage } = useInfiniteQuery({
+  queryKey: ['trades'],
+  queryFn: async ({ pageParam = 1 }) => {
+    const response = await getTrades(pageParam, rpp);
     return response.data;
+  },
+  initialPageParam: 1,
+  getNextPageParam: (lastPage, allPages) => {
+    return lastPage.more ? allPages.length + 1 : undefined;
   },
 });
 
-const hasMore = ref(false);
-
-const trades = computed(() => allTrades.value);
+const trades = computed(() => data.value?.pages.flatMap((page) => page.list) ?? []);
 
 const loadMoreTrades = () => {
-  currentPage.value += 1;
+  void fetchNextPage();
 };
 </script>
 
